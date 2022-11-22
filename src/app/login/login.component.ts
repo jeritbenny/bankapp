96fdc2b23d025e6f1eff0c7f6730ec3876a5d11e
jeritbenny <jeritbenny.jbk@gmail.com>
2022-11-22 11:26:42 +0530

    bank app

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  aim="your perfect banking panter";
+
+  account="enter the acc_no";
+  acno=''
+  pswd=''
+
+  userDetails:any={
+   1001:{acno:1001,username:"jerry",password:1000,balance:1000},
+   1002:{acno:1002,username:"jerit",password:1001,balance:1000},
+   1003:{acno:1003,username:"jerin",password:1002,balance:1000}
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  acnoChange(event:any){
+    console.log(event);
+
+    this.acno=event.target.value;
+    console.log(this.acno);
+    
+  }
+  pswdChange(event:any){
+    this.pswd=event.target.value;
+    console.log(this.pswd);
+    
+  }
+
+  login(){
+    //alert("login clicked");
+    var acno=this.acno;
+    var pswd=this.pswd;
+    var userDetails=this.userDetails;
+    if(acno in userDetails){
+      if(pswd==userDetails[acno]['password']){
+        alert("login sussessful");
+      }
+      else{
+        alert('invalid password');
+      }
+    }
+    else{
+      alert('Invalid user details');
+    }
+  }
+
+}
